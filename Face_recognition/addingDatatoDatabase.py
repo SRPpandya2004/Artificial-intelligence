c1c34bc1d52b3f3425e7c7844831a705ef9a11ba
Shaunak Pandya <176204036+SRPpandya2004@users.noreply.github.com>
2025-06-06 11:45:02 +0530

    Update addingDatatoDatabase.py

--- Face_recognition/addingDatatoDatabase.py
+++ Face_recognition/addingDatatoDatabase.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
     'databaseURL':"______Add URL here____________"
